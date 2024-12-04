2686c6a86c70cafa96e856ef26be2bc3067a153f
Jen Moriarty <JMoriarty78@gmail.com>
2024-12-04 16:48:54 -0700

    Create player.h

---
+++ player.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "level.h"
+using namespace sf;
+
+class Player {
+private:
+    float x;        
+    float y;        
+    float vx; 
+    float vy; 
+    float speed;    
+    float size;     
+
+public:
+    
+    Player(float startX, float startY); // Constructor
+    void handleInput();
+    void update(float deltaTime, const Level& level);
+    void draw(RenderWindow& window);
+
+};
+
+
